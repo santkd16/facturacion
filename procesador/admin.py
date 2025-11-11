69d86e1e47dfd52b2cb91d5d252a2c553c42be6a
DAVID SANTIAGO PIEDRAHITA CABEZA <145588434+santkd16@users.noreply.github.com>
2025-11-11 12:24:40 -0500

    Implementar módulo de liquidación con catálogos contables

--- procesador/admin.py
+++ procesador/admin.py
@@ -1,24 +1,48 @@
 from django.contrib import admin
 from .models import (
-    Proveedor,
+    CuentaContable,
+    CuentaContableProveedor,
+    Empresa,
     FacturaXML,
     FacturaXLS,
-    CuentaContable,
-    Retencion,
-    TarifaICA,
+    PermisoEmpresa,
+    Proveedor,
 )
+
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "nit", "activo")
+    list_filter = ("activo",)
+    search_fields = ("nombre", "nit")
+
+
+@admin.register(PermisoEmpresa)
+class PermisoEmpresaAdmin(admin.ModelAdmin):
+    list_display = ("usuario", "empresa", "es_administrador")
+    list_filter = ("empresa", "es_administrador")
+    search_fields = ("usuario__username", "usuario__email", "empresa__nombre")
 
 
 @admin.register(Proveedor)
 class ProveedorAdmin(admin.ModelAdmin):
-    list_display = ("nombre", "nit")
+    list_display = ("nombre", "nit", "empresa")
+    list_filter = ("empresa",)
     search_fields = ("nombre", "nit")
 
 
 @admin.register(FacturaXML)
 class FacturaXMLAdmin(admin.ModelAdmin):
-    list_display = ("cufe", "fecha", "proveedor", "subtotal", "iva", "total")
-    list_filter = ("fecha", "proveedor")
+    list_display = (
+        "cufe",
+        "fecha",
+        "proveedor",
+        "empresa",
+        "subtotal",
+        "iva",
+        "total",
+    )
+    list_filter = ("fecha", "proveedor", "empresa")
 
 
 @admin.register(FacturaXLS)
@@ -28,13 +52,14 @@ class FacturaXLSAdmin(admin.ModelAdmin):
         "cufe",
         "nit_emisor",
         "nombre_emisor",
+        "empresa",
         "iva",
         "inc",
         "total",
         "activo",
     )
     search_fields = ("cufe", "nit_emisor", "nombre_emisor")
-    list_filter = ("activo", "tipo_documento")
+    list_filter = ("activo", "tipo_documento", "empresa")
 
 
 @admin.register(CuentaContable)
@@ -43,13 +68,29 @@ class CuentaContableAdmin(admin.ModelAdmin):
     search_fields = ("codigo", "descripcion")
 
 
-# Nuevos registros
-@admin.register(Retencion)
-class RetencionAdmin(admin.ModelAdmin):
-    list_display = ("proveedor", "porcentaje", "cuenta_contable")
-    list_filter = ("proveedor",)
-
-
-@admin.register(TarifaICA)
-class TarifaICAAdmin(admin.ModelAdmin):
-    list_display = ("valor", "descripcion")
+@admin.register(CuentaContableProveedor)
+class CuentaContableProveedorAdmin(admin.ModelAdmin):
+    list_display = (
+        "proveedor",
+        "casilla",
+        "cuenta",
+        "naturaleza",
+        "porcentaje",
+        "modo_calculo",
+        "activo",
+    )
+    list_filter = (
+        "proveedor__empresa",
+        "proveedor",
+        "casilla",
+        "naturaleza",
+        "modo_calculo",
+        "activo",
+    )
+    search_fields = (
+        "proveedor__nombre",
+        "proveedor__nit",
+        "cuenta__codigo",
+        "cuenta__descripcion",
+    )
+    autocomplete_fields = ("proveedor", "cuenta")
